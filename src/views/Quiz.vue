138b1bd3987b2dc614a8a924cb5c38141b29742e
Robbe Heremans <r0743799@student.thomasmore>
2021-09-16 01:29:57 +0200

    Full working of Quiz page

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,30 +1,37 @@
 <template>
-  <ContentContainer :border="true" :black-bg="true">
-    <div v-if="questionCount !== questions.length">
-      <p>{{ questionCount }}. {{ currentQuestion.question }}</p>
-      <div v-if="!review">
-        <button v-for="answer in currentQuestion.possibleAnswers"
-                :key="answer.id"
-                @click="answered(answer.id)">
-          {{ answer.text }}
-        </button>
-      </div>
-      <div v-else>
-        <p v-for="answer in currentQuestion.possibleAnswers"
-           :key="answer.id">
-          {{ answer.id === currentQuestion.correctAnswer ? 'V' : 'X' }}  {{ answer.text }}
-        </p>
-        <button @click="next">next</button>
-      </div>
-    </div>
-    <div v-else>
-      <p>Score: {{ score }}</p>
+  <ContentContainer :black-bg="true">
+    <div>
+      <template v-if="questionCount !== questions.length">
+        <table>
+          <tr>
+            <th>Q{{ questionCount }}.</th>
+            <th>
+              <span v-html="currentQuestion.question"></span><br>
+              <img v-if="typeof currentQuestion.image !== 'undefined'" :src="currentQuestion.image" alt="'Image'" height="100px" width="auto" />
+            </th>
+          </tr>
+          <tr v-for="answer in currentQuestion.possibleAnswers"
+              :key="answer.id"
+              @click="!review ? answered(answer.id) : null">
+            <td>{{ answer.id }}.</td>
+            <td :class="{
+                wrong: isWrong(answer),
+                correct: isCorrect(answer)
+            }">{{ answer.text }}</td>
+          </tr>
+        </table>
+        <button v-if="review" @click="next">next</button>
+      </template>
+      <template v-else>
+        <p>Score: {{ score }}</p>
+      </template>
     </div>
   </ContentContainer>
 </template>
 
 <script>
 import ContentContainer from "@/components/ContentContainer";
+const imageResetCharges = require("../assets/img/reset-charges.jpg");
 
 export default {
   name: "Quiz",
@@ -34,23 +41,52 @@ export default {
   data() {
     const questions = [
           {
-            question: 'question',
-            correctAnswer: 0,
+            question: 'What happens when a Nexus event branches past red line?',
+            correctAnswer: 'B',
             possibleAnswers: [
-              {id: 0, text: 'answer 1'},
-              {id: 1, text: 'answer 2'},
-              {id: 2, text: 'answer 3'},
+              {id: 'A', text: 'Very bad things.'},
+              {id: 'B', text: 'It\'s when the TVA can no longer reset the Nexus event.'},
             ]
           },
           {
-            question: 'question',
-            correctAnswer: 2,
+            question: 'Thanos has two apples. He eats both but realizes he wants more. He goes back in time 20 minutes' +
+                'and eats the apples again. <br/> Does this mean the apples will not have existed in the timeline he left?',
+            correctAnswer: 'B',
             possibleAnswers: [
-              {id: 0, text: 'answer 1'},
-              {id: 1, text: 'answer 2'},
-              {id: 2, text: 'answer 3'},
+              {id: 'A', text: 'No, because time is constantly happening.'},
+              {id: 'B', text: 'The question doesn\'t matter because a branch cannot change another time branch.'},
+              {id: 'C', text: 'Thanos would\'ve been hungry prior because the Grandfather paradox already accounted' +
+                    'for the change in matter before its move.'},
             ]
-          }
+          },
+          {
+            //question: 'Why do we need to show up in real time on a nexus event?',
+            question: 'Why don\'t we travel back to before an attack on our huntsmen when the variant first arrives?',
+            correctAnswer: 'A',
+            possibleAnswers: [
+              {id: 'A', text: 'Nexus events destabilise the time flow. After all are these branches still changing and growing.'},
+              {id: 'B', text: 'It would create a Grandfather paradox where sending the first squad would have been unnecessary' +
+                    'and the second one never occurring.'},
+            ]
+          },
+          {
+            question: 'What are these?',
+            image: imageResetCharges,
+            correctAnswer: 'C',
+            possibleAnswers: [
+              {id: 'A', text: 'Hip flasks'},
+              {id: 'B', text: 'Smoke grenades'},
+              {id: 'C', text: 'Reset charges'},
+            ]
+          },
+          {
+            question: 'What do these do?',
+            correctAnswer: ['A', 'B'],
+            possibleAnswers: [
+              {id: 'A', text: 'Prune the effected radius of a branch\'s timeline, allowing time to heal all its wounds.'},
+              {id: 'B', text: 'Disintegrate everything in its vicinity.'},
+            ]
+          },
         ];
 
     return {
@@ -58,13 +94,21 @@ export default {
       questionCount: 0,
       questions: questions,
       currentQuestion: questions[0],
+      selectedQuestion: 0,
       review: false
     }
   },
   methods: {
+    isWrong(answer) {
+      return this.review && (this.selectedQuestion === answer.id && (answer.id !== this.currentQuestion.correctAnswer || !this.currentQuestion.correctAnswer.includes(answer.id)));
+    },
+    isCorrect(answer) {
+      return this.review && (answer.id === this.currentQuestion.correctAnswer || this.currentQuestion.correctAnswer.includes(answer.id));
+    },
     answered(id) {
       if (id === this.currentQuestion.correctAnswer) this.score++;
       this.review = true;
+      this.selectedQuestion = id;
     },
     next() {
       if (++this.questionCount !== this.questions.length) this.currentQuestion = this.questions[this.questionCount];
@@ -75,5 +119,32 @@ export default {
 </script>
 
 <style scoped>
-
+div:nth-child(2) {
+  position: absolute;
+  inset: 10px;
+  border: 1px solid var(--main-color-lighten);
+}
+table {
+  width: 100%;
+}
+tr {
+  vertical-align: top;
+}
+tr:first-child {
+  border-bottom: 1px solid var(--main-color-lighten);
+}
+th:first-child {
+  padding-right: 20px !important;
+}
+th, td {
+  padding: 5px 10px !important;
+}
+.correct {
+  background-color: var(--main-color-lighten);
+  color: black;
+}
+.wrong {
+  background-color: red;
+  color: black;
+}
 </style>
